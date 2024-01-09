23784518707dbc94db27a42b5cd02a3f625449e5
Alexey Abramov <qustt@yandex.ru>
2024-01-09 20:16:32 +0300

    1attempt

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+// Выбираем шаблон карточки и контейнер для вставки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+//Создаем функцию добавления карты
+function addCard() {
+    for (let i = 0; i < initialCards.length; i++) {
+        const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+        cardElement.querySelector('.card__image').src = initialCards[i].link;
+        cardElement.querySelector('.card__title').textContent = initialCards[i].name;
+        cardList.append(cardElement);
+        const deleteButton = cardElement.querySelector('.card__delete-button');
+        deleteButton.addEventListener('click', deleteCard);
+    }
+} 
+//Создаем функцию удаления карты
 
-// @todo: Функция удаления карточки
+function deleteCard (button) {
+    const card = button.target.closest('.card');
+    card.remove();
+}
+//Вызываем функцию добавления карт на страницу
+addCard();
 
-// @todo: Вывести карточки на страницу
